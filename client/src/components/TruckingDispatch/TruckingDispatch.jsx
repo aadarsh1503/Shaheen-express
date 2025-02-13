59535a8fb8417d14f75dc1ddd769e8290e4e29a9
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-02-13 14:55:00 +0530

    done

--- client/src/components/TruckingDispatch/TruckingDispatch.jsx
+++ client/src/components/TruckingDispatch/TruckingDispatch.jsx
@@ -23,7 +23,7 @@ const TruckingDispatch = () => {
         </div>
 
         {/* Text Content */}
-        <div className="w-1/2 relative top-32 p-8">
+        <div className="w-1/2 relative top-32 font-noto-sans-display p-8">
           <p className="text-gray-600 mb-5">Trucking & Dispatch</p>
           <h2 className="text-3xl mb-5 font-semibold text-[#264795]">
           Trucking And Dispatch Services
